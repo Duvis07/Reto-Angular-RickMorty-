376861d054ff3e70ed2394232031c6e565dfabfe
duvan.botero <duvis07>
2022-10-03 11:46:51 -0500

    Se crea estructura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { MainComponent } from './components/main/main.component';
+import { RickandmortyComponent } from './components/rickandmorty/rickandmorty.component';
+import { FooterComponent } from './components/main/footer/footer.component';
+import { HeaderComponent } from './components/main/header/header.component';
+import { LayoutComponent } from './components/main/layout/layout.component';
+import { MenuComponent } from './components/main/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    MainComponent,
+    RickandmortyComponent,
+    FooterComponent,
+    HeaderComponent,
+    LayoutComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
