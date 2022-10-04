4df009100029d0bba85dc16984d9f1e53114f2c1
duvan.botero <duvis07>
2022-10-04 10:05:00 -0500

    Se agrega Alert 2

--- src/app/components/characters/characters.component.ts
+++ src/app/components/characters/characters.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { take } from 'rxjs';
 import { Character } from 'src/app/shared/interfaces/Character';
 import { RickMortyService } from 'src/app/shared/services/rick-morty.service';
+import { MessageService, Message } from 'primeng/api';
+import Swal from 'sweetalert2';
 
 type RequestInfo = {
   next: string | null;
@@ -11,17 +13,16 @@ type RequestInfo = {
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
-  styleUrls: ['./characters.component.css']
+  styleUrls: ['./characters.component.css'],
 })
 export class CharactersComponent implements OnInit {
-
   info: RequestInfo = {
     next: null,
   };
 
   characters: Character[] = [];
 
-  newArray: String[] = []
+  newArray: String[] = [];
 
   page: number = 0;
 
@@ -30,14 +31,15 @@ export class CharactersComponent implements OnInit {
   private pageNum = 1;
 
   private query: string = '';
+
   
+
   constructor(
-    private characterSvc: RickMortyService,
-    ) {
-   }
+    private characterSvc: RickMortyService
+  ) {}
 
   ngOnInit(): void {
-    this.getDataFromService()
+    this.getDataFromService();
   }
 
   private getDataFromService(): void {
@@ -49,17 +51,23 @@ export class CharactersComponent implements OnInit {
           const { info, results } = res;
           this.characters = [...this.characters, ...results];
           this.info = info;
-          this.characters.map(char => {
-            this.newArray.push(char.name)
-          })
+          this.characters.map((char) => {
+            this.newArray.push(char.name);
+          });
         } else {
           this.characters = [];
         }
-      })
-    }
-
-  addCharacter(name: string){
-    this.newArray.push(name)
+      });
   }
 
+  addCharacter(name: string) {
+    this.newArray.push(name);
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Personaje Agregado Correctamente',
+      showConfirmButton: false,
+      timer: 1000
+    })
+  }
 }
